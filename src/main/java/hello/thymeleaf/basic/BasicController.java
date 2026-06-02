dfd7f83d524a44f606aa0354728329fbad692e18
Jin Young Kim <wlsdud66523@kakao.com>
2026-06-02 12:17:51 +0900

    thymleaf-注釈

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -129,6 +129,12 @@ public class BasicController {
         model.addAttribute("users",list);
     }
 
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data","Spring!");
+        return "/basic/commnets";
+    }
+
     @Data
     public static class User {
         private String username;
