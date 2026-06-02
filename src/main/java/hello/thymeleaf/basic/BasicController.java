a65bd8face41919b6d5daaf76772916de67514db
Jin Young Kim <wlsdud66523@kakao.com>
2026-06-02 11:14:10 +0900

    thymleaf-演算子

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -97,6 +97,12 @@ public class BasicController {
         return "/basic/literal";
     }
 
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData",null);
+        model.addAttribute("data","Spring!");
+        return "/basic/operation";
+    }
 
     @Data
     public static class User {
