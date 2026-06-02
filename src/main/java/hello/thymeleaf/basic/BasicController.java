99ae3660e4056269dff004bf6b99b6714b1e294f
Jin Young Kim <wlsdud66523@kakao.com>
2026-06-02 11:51:19 +0900

    thymleaf-反復

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -109,6 +109,21 @@ public class BasicController {
         return "/basic/operation";
     }
 
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "/basic/each";
+    }
+
+    private void addUsers(Model model) {
+        ArrayList<Object> list = new ArrayList<>();
+        list.add(new User("UserA",10));
+        list.add(new User("UserB",20));
+        list.add(new User("UserB",30));
+        model.addAttribute("users",list);
+    }
+
+
     @Data
     public static class User {
         private String username;
